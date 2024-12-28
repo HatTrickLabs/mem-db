3ffca07973782fdfd40e524f275687dd34a56760
Jerrod Eiman <jerrod.eiman@hattricklabs.com>
2024-12-28 11:59:31 -0600

    Cleanup obsolete commented code.

--- src/HatTrick.MemDb/MemDbSerializationCloner.cs
+++ src/HatTrick.MemDb/MemDbSerializationCloner.cs
@@ -19,23 +19,6 @@ namespace HatTrick.InMemDb
         #endregion
 
         #region deep copy
-        //public T DeepCopy(T value)
-        //{
-        //    T rec = default(T);
-        //    using MemoryStream ms = new MemoryStream();
-        //    using (BinaryWriter writer = new BinaryWriter(ms, Encoding.UTF8, true))
-        //    {
-        //        _serializer.Serialize(value, writer);
-        //    }
-        //    int length = (int)ms.Position;
-        //    ms.Position = 0;
-        //    using (BinaryReader reader = new BinaryReader(ms, Encoding.UTF8, true))
-        //    {
-        //        rec = _serializer.Deserialize(reader, length);
-        //    }
-        //    return rec;
-        //}
-
         public T DeepCopy(T value)
         {
             byte[] raw = _serializer.Serialize(value);
@@ -58,23 +41,6 @@ namespace HatTrick.InMemDb
 
             return clones;
         }
-
-        //public T[] DeepCopy(IList<T> values)
-        //{
-        //    T[] newValues = new T[values.Count];
-        //    using MemoryStream ms = new MemoryStream();
-        //    using BinaryWriter writer = new BinaryWriter(ms, Encoding.UTF8, true);
-        //    using BinaryReader reader = new BinaryReader(ms, Encoding.UTF8, true);
-        //    for (int i = 0; i < values.Count; i++)
-        //    {
-        //        _serializer.Serialize(values[i], writer);
-        //        int length = (int)ms.Position;
-        //        ms.Position = 0;
-        //        newValues[i] = _serializer.Deserialize(reader, length);
-        //        ms.Position = 0;
-        //    }
-        //    return newValues;
-        //}
         #endregion
     }
 }
