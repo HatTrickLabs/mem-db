261ac0018a06534c073ea003505a0814d77748f7
Jerrod Eiman <jerrod.eiman@hattricklabs.com>
2025-03-08 22:30:06 -0600

    Tests WIP

---
+++ test/HatTrick.MemDb.TestHarness/Exception/NotNullException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HatTrick.InMemDb.TestHarness
+{
+    public class NotNullException : Exception
+    {
+        #region internals
+        private Type _type;
+        private object _value;
+        #endregion
+
+        #region ctors
+        public NotNullException(Type type, object value)
+            : base($"{type.Name} {value} is not null")
+        {
+            _type = type;
+            _value = value;
+        }
+        #endregion
+    }
+}
