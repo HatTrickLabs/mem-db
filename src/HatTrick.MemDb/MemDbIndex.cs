e3394676aec9f9fd16566292c17983308ed5baa3
Jerrod Eiman <jerrod.eiman@hattricklabs.com>
2025-05-17 21:24:00 -0500

    WIP

--- src/HatTrick.MemDb/MemDbIndex.cs
+++ src/HatTrick.MemDb/MemDbIndex.cs
@@ -6,12 +6,13 @@ namespace HatTrick.InMemDb
     public class MemDbIndex<T,Y> where T : class
     {
         #region internals
+        private string _name;
         private Func<T, Y> _keyResolver;
         private Dictionary<Y, List<int>> _index;
         #endregion
 
         #region ctors
-        public MemDbIndex(Func<T, Y> keyResolver)
+        public MemDbIndex(string Name, Func<T, Y> keyResolver)
         {
             _keyResolver = keyResolver ?? throw new ArgumentNullException(nameof(keyResolver));
         }
@@ -25,17 +26,35 @@ namespace HatTrick.InMemDb
         #endregion
 
         #region apply
-        public void Apply(T record)
+        public void Apply(T record, int pointer)
         {
-
+            Y key = _keyResolver(record);
+            List<int> set = null;
+            if (_index.TryGetValue(key, out set))
+            {
+                set.Add(pointer);
+            }
+            else
+            {
+                set = new List<int>() { pointer };
+                _index.Add(key, set);
+            }
         }
         #endregion
 
         #region remove
         public void Remove(T record)
         {
-
+            _index.Remove(_keyResolver(record));
         }
         #endregion
+
+        public int[] ResolvePointers(Y key)
+        {
+            if (_index.TryGetValue(key, out List<int> set))
+                return set.ToArray();
+
+            return Array.Empty<int>();
+        }
     }
 }
